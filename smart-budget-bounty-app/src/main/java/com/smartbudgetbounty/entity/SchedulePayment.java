5ab8ed87eeefb5182ec11ce2e044a2dbf2fc6fbe
Marcus Tay <tay.yanglinmarcus@hcltech.com>
2025-06-24 10:42:13 +0800

    feat: implemented schedule POST and GET Api

--- smart-budget-bounty-app/src/main/java/com/smartbudgetbounty/entity/SchedulePayment.java
+++ smart-budget-bounty-app/src/main/java/com/smartbudgetbounty/entity/SchedulePayment.java
@@ -2,6 +2,8 @@ package com.smartbudgetbounty.entity;
 
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,10 +17,31 @@ public class SchedulePayment {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	private String billName;
-	private Double billAmount;
+	// Common fields
+	private String recipientName;
+	private String paymentMethod;
+	
+	// Giro
+	private String bankName;
+	private String accountNumber;
+	private Double transferLimit;
+	
+	// Standing Instruction
+	@JsonFormat(pattern = "dd-MM-yyyy", timezone = "Asia/Singapore")
 	private Instant startDate;
-	private String recurrence;
+	@JsonFormat(pattern = "dd-MM-yyyy", timezone = "Asia/Singapore")
+	private Instant endDate;
+	
+	private String frequency;
+	
+	// Credit/Debit Card
+	private String cardNumber;
+	private String nameOnCard;
+	
+	@JsonFormat(pattern = "dd-MM-yyyy", timezone = "Asia/Singapore")
+	private Instant expiryDate;
+	
+	private String cvv;
 	
 	@ManyToOne
 	@JoinColumn(name = "user_id")  // owning side: FK is here
@@ -27,14 +50,24 @@ public class SchedulePayment {
 	//TODO1: Yet to add 1 more r/s to Rewards Table
 	
 	public SchedulePayment() {}
-	
-	public SchedulePayment(String billName, Double billAmount,
-			Instant startDate, String recurrence, User user) {
+
+	public SchedulePayment(Long id, String recipientName, String paymentMethod, String bankName,
+			String accountNumber, Double transferLimit, Instant startDate, Instant endDate, String frequency,
+			String cardNumber, String nameOnCard, Instant expiryDate, String cvv, User user) {
 		super();
-		this.billName = billName;
-		this.billAmount = billAmount;
+		this.id = id;
+		this.recipientName = recipientName;
+		this.paymentMethod = paymentMethod;
+		this.bankName = bankName;
+		this.accountNumber = accountNumber;
+		this.transferLimit = transferLimit;
 		this.startDate = startDate;
-		this.recurrence = recurrence;
+		this.endDate = endDate;
+		this.frequency = frequency;
+		this.cardNumber = cardNumber;
+		this.nameOnCard = nameOnCard;
+		this.expiryDate = expiryDate;
+		this.cvv = cvv;
 		this.user = user;
 	}
 
@@ -46,20 +79,44 @@ public class SchedulePayment {
 		this.id = id;
 	}
 
-	public String getBillName() {
-		return billName;
+	public String getRecipientName() {
+		return recipientName;
 	}
 
-	public void setBillName(String billName) {
-		this.billName = billName;
+	public void setRecipientName(String recipientName) {
+		this.recipientName = recipientName;
 	}
 
-	public Double getBillAmount() {
-		return billAmount;
+	public String getPaymentMethod() {
+		return paymentMethod;
 	}
 
-	public void setBillAmount(Double billAmount) {
-		this.billAmount = billAmount;
+	public void setPaymentMethod(String paymentMethod) {
+		this.paymentMethod = paymentMethod;
+	}
+
+	public String getBankName() {
+		return bankName;
+	}
+
+	public void setBankName(String bankName) {
+		this.bankName = bankName;
+	}
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public Double getTransferLimit() {
+		return transferLimit;
+	}
+
+	public void setTransferLimit(Double transferLimit) {
+		this.transferLimit = transferLimit;
 	}
 
 	public Instant getStartDate() {
@@ -70,14 +127,52 @@ public class SchedulePayment {
 		this.startDate = startDate;
 	}
 
-	
-
-	public String getRecurrence() {
-		return recurrence;
+	public Instant getEndDate() {
+		return endDate;
 	}
 
-	public void setRecurrence(String recurrence) {
-		this.recurrence = recurrence;
+	public void setEndDate(Instant endDate) {
+		this.endDate = endDate;
+	}
+
+	public String getFrequency() {
+		return frequency;
+	}
+
+	public void setFrequency(String frequency) {
+		this.frequency = frequency;
+	}
+
+	public String getCardNumber() {
+		return cardNumber;
+	}
+
+	public void setCardNumber(String cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+
+	public String getNameOnCard() {
+		return nameOnCard;
+	}
+
+	public void setNameOnCard(String nameOnCard) {
+		this.nameOnCard = nameOnCard;
+	}
+
+	public Instant getExpiryDate() {
+		return expiryDate;
+	}
+
+	public void setExpiryDate(Instant expiryDate) {
+		this.expiryDate = expiryDate;
+	}
+
+	public String getCvv() {
+		return cvv;
+	}
+
+	public void setCvv(String cvv) {
+		this.cvv = cvv;
 	}
 
 	public User getUser() {
@@ -87,8 +182,6 @@ public class SchedulePayment {
 	public void setUser(User user) {
 		this.user = user;
 	}
-	
-	
 	
 }
 
