808c73c203f0623d73175b6729bf46bf1af262c4
Natthan Lee <natthan.lee@hcltech.com>
2025-06-25 21:54:04 +0800

    chore: formatted Transaction

--- smart-budget-bounty-app/src/main/java/com/smartbudgetbounty/entity/Transaction.java
+++ smart-budget-bounty-app/src/main/java/com/smartbudgetbounty/entity/Transaction.java
@@ -11,158 +11,168 @@ import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Transaction {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private Double transactionAmount;
-	private Instant createdAt;
-	private String recipientName;
-	
-	private String paymentMethod;
-	private String paynowRecipient;
-	private String accountNumber;
-	private String remarks;
-	
-	private Instant transferDate;
-	
-	@ManyToOne // Owner - Because in a one-many r/s, the many is the owner
-	@JoinColumn(name = "user_id")
-	private User user;
-	
-	@ManyToOne // Owner - Because in a one-many r/s, the many is the owner
-	@JoinColumn(name = "payment_method_id", nullable = false, referencedColumnName="id")
-	private PaymentMethod paymentMethod2;
-	
-	
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Double transactionAmount;
+    private Instant createdAt;
+    private String recipientName;
+
+    private String paymentMethod;
+    private String paynowRecipient;
+    private String accountNumber;
+    private String remarks;
+
+    private Instant transferDate;
+
+    @ManyToOne // Owner - Because in a one-many r/s, the many is the owner
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne // Owner - Because in a one-many r/s, the many is the owner
+    @JoinColumn(name = "payment_method_id", nullable = false, referencedColumnName = "id")
+    private PaymentMethod paymentMethod2;
+
 //	@OneToOne // Owner - Because Transaction exist then can have rewards
 //	@JoinColumn(name="reward_id")
 //	private Reward reward;
-	
+
 //	@OneToOne // Owner - Because Transaction is created for an Account, not the other way around
 //	@JoinColumn(name="account_id")
 //	private Account account;
 
-	public Transaction() {
-		super();
-	}
+    public Transaction() {
+        super();
+    }
 
-	public Transaction(Double transactionAmount, Instant createdAt,
-		String recipientName, String paymentMethod, String paynowRecipient, 
-		String accountNumber, String remarks,
-		Instant transferDate, User user) {
-		super();
-		this.transactionAmount = transactionAmount;
-		this.createdAt = createdAt;
-		this.recipientName = recipientName;
-		this.paymentMethod = paymentMethod;
-		this.paynowRecipient = paynowRecipient;
-		this.accountNumber = accountNumber;
-		this.remarks = remarks;
-		this.transferDate = transferDate;
-		this.user = user;
-	}
+    public Transaction(
+        Double transactionAmount,
+        Instant createdAt,
+        String recipientName,
+        String paymentMethod,
+        String paynowRecipient,
+        String accountNumber,
+        String remarks,
+        Instant transferDate,
+        User user
+    ) {
+        super();
+        this.transactionAmount = transactionAmount;
+        this.createdAt = createdAt;
+        this.recipientName = recipientName;
+        this.paymentMethod = paymentMethod;
+        this.paynowRecipient = paynowRecipient;
+        this.accountNumber = accountNumber;
+        this.remarks = remarks;
+        this.transferDate = transferDate;
+        this.user = user;
+    }
 
+    public Long getId() {
+        return id;
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public Double getTransactionAmount() {
+        return transactionAmount;
+    }
 
-	public Double getTransactionAmount() {
-		return transactionAmount;
-	}
+    public void setTransactionAmount(Double transactionAmount) {
+        this.transactionAmount = transactionAmount;
+    }
 
-	public void setTransactionAmount(Double transactionAmount) {
-		this.transactionAmount = transactionAmount;
-	}
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
 
-	public Instant getCreatedAt() {
-		return createdAt;
-	}
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
 
-	public void setCreatedAt(Instant createdAt) {
-		this.createdAt = createdAt;
-	}
+    public User getUser() {
+        return user;
+    }
 
-	public User getUser() {
-		return user;
-	}
+    public void setUser(User user) {
+        this.user = user;
+    }
 
-	public void setUser(User user) {
-		this.user = user;
-	}
+    public String getRecipientName() {
+        return recipientName;
+    }
 
-	public String getRecipientName() {
-		return recipientName;
-	}
+    public void setRecipientName(String recipientName) {
+        this.recipientName = recipientName;
+    }
 
-	public void setRecipientName(String recipientName) {
-		this.recipientName = recipientName;
-	}
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
 
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
 
-	public String getPaymentMethod() {
-		return paymentMethod;
-	}
+    public String getPaynowRecipient() {
+        return paynowRecipient;
+    }
 
+    public void setPaynowRecipient(String paynowRecipient) {
+        this.paynowRecipient = paynowRecipient;
+    }
 
-	public void setPaymentMethod(String paymentMethod) {
-		this.paymentMethod = paymentMethod;
-	}
+    public String getAccountNumber() {
+        return accountNumber;
+    }
 
-	public String getPaynowRecipient() {
-		return paynowRecipient;
-	}
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
 
+    public String getRemarks() {
+        return remarks;
+    }
 
-	public void setPaynowRecipient(String paynowRecipient) {
-		this.paynowRecipient = paynowRecipient;
-	}
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
 
+    public Instant getTransferDate() {
+        return transferDate;
+    }
 
-	public String getAccountNumber() {
-		return accountNumber;
-	}
+    public void setTransferDate(Instant transferDate) {
+        this.transferDate = transferDate;
+    }
 
+    @Override
+    public String toString() {
+        return "Transaction [id="
+            + id
+            + ", transactionAmount="
+            + transactionAmount
+            + ", createdAt="
+            + createdAt
+            + ", recipientName="
+            + recipientName
+            + ", paymentMethod="
+            + paymentMethod
+            + ", paynowRecipient="
+            + paynowRecipient
+            + ", accountNumber="
+            + accountNumber
+            + ", remarks="
+            + remarks
+            + ", transferDate="
+            + transferDate
+            + ", user="
+            + user
+            + ", paymentMethod2="
+            + paymentMethod2
+            + "]";
+    }
 
-	public void setAccountNumber(String accountNumber) {
-		this.accountNumber = accountNumber;
-	}
-
-
-	public String getRemarks() {
-		return remarks;
-	}
-
-
-	public void setRemarks(String remarks) {
-		this.remarks = remarks;
-	}
-
-
-	public Instant getTransferDate() {
-		return transferDate;
-	}
-
-
-	public void setTransferDate(Instant transferDate) {
-		this.transferDate = transferDate;
-	}
-
-	@Override
-	public String toString() {
-		return "Transaction [id=" + id + ", transactionAmount="
-				+ transactionAmount + ", createdAt=" + createdAt
-				+ ", recipientName=" + recipientName + ", paymentMethod="
-				+ paymentMethod + ", paynowRecipient=" + paynowRecipient
-				+ ", accountNumber=" + accountNumber + ", remarks=" + remarks
-				+ ", transferDate=" + transferDate + ", user=" + user
-				+ ", paymentMethod2=" + paymentMethod2 + "]";
-	}
-	
-	
 }
