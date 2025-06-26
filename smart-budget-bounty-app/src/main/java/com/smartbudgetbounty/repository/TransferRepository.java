15c4ac8a61887678671857a29e2df9d1f0400b7e
Natthan Lee <natthan.lee@hcltech.com>
2025-06-26 15:15:36 +0800

    chore: minor formatting

--- smart-budget-bounty-app/src/main/java/com/smartbudgetbounty/repository/TransferRepository.java
+++ smart-budget-bounty-app/src/main/java/com/smartbudgetbounty/repository/TransferRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.smartbudgetbounty.entity.Transfer;
 
-public interface TransferRepository extends JpaRepository<Transfer, Long>{
+public interface TransferRepository extends JpaRepository<Transfer, Long> {
 
 }
