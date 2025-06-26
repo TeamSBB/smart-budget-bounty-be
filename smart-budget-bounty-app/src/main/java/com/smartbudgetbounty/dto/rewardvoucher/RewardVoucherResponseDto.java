899ad350cd148d894c10088632b99df1d35a851f
Natthan Lee <natthan.lee@hcltech.com>
2025-06-26 10:49:14 +0800

    feat: initialized RewardVoucherResponseDto

---
+++ smart-budget-bounty-app/src/main/java/com/smartbudgetbounty/dto/rewardvoucher/RewardVoucherResponseDto.java
@@ -0,0 +1,92 @@
+package com.smartbudgetbounty.dto.rewardvoucher;
+
+import java.time.Instant;
+
+public class RewardVoucherResponseDto {
+    private Long id;
+    private String voucherStatus;
+    private Double discount;
+    private Instant earnDate;
+    private Instant redeemDate;
+    private Long userId;
+    private Long pointsTransactionId;
+
+    public RewardVoucherResponseDto() {
+        super();
+    }
+
+    public RewardVoucherResponseDto(
+        Long id,
+        String voucherStatus,
+        Double discount,
+        Instant earnDate,
+        Instant redeemDate,
+        Long userId,
+        Long pointsTransactionId
+    ) {
+        super();
+        this.id = id;
+        this.voucherStatus = voucherStatus;
+        this.discount = discount;
+        this.earnDate = earnDate;
+        this.redeemDate = redeemDate;
+        this.userId = userId;
+        this.pointsTransactionId = pointsTransactionId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getVoucherStatus() {
+        return voucherStatus;
+    }
+
+    public void setVoucherStatus(String voucherStatus) {
+        this.voucherStatus = voucherStatus;
+    }
+
+    public Double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Double discount) {
+        this.discount = discount;
+    }
+
+    public Instant getEarnDate() {
+        return earnDate;
+    }
+
+    public void setEarnDate(Instant earnDate) {
+        this.earnDate = earnDate;
+    }
+
+    public Instant getRedeemDate() {
+        return redeemDate;
+    }
+
+    public void setRedeemDate(Instant redeemDate) {
+        this.redeemDate = redeemDate;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getPointsTransactionId() {
+        return pointsTransactionId;
+    }
+
+    public void setPointsTransactionId(Long pointsTransactionId) {
+        this.pointsTransactionId = pointsTransactionId;
+    }
+}
