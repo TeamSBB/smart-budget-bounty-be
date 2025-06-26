cfa6bce5dc276333f2212479a0345c21be04e77c
Natthan Lee <natthan.lee@hcltech.com>
2025-06-26 11:40:23 +0800

    feat: added super() in constructor for consistency

--- smart-budget-bounty-app/src/main/java/com/smartbudgetbounty/dto/rewardpointstransaction/CreateRedeemRewardPointsTransactionRequestDto.java
+++ smart-budget-bounty-app/src/main/java/com/smartbudgetbounty/dto/rewardpointstransaction/CreateRedeemRewardPointsTransactionRequestDto.java
@@ -4,6 +4,7 @@ public class CreateRedeemRewardPointsTransactionRequestDto {
     private Integer redeemAmount;
 
     public CreateRedeemRewardPointsTransactionRequestDto(Integer redeemAmount) {
+        super();
         this.redeemAmount = redeemAmount;
     }
 
