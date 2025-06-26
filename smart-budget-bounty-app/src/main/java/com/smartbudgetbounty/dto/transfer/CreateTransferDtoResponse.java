734e3321fdb795d14e21a638973f2a999d3913ae
joel ong <ong.jiaquanjoel@hcltech.com>
2025-06-26 18:13:14 +0800

    enhance(transfer): Transfer table needed more info, added column

--- smart-budget-bounty-app/src/main/java/com/smartbudgetbounty/dto/transfer/CreateTransferDtoResponse.java
+++ smart-budget-bounty-app/src/main/java/com/smartbudgetbounty/dto/transfer/CreateTransferDtoResponse.java
@@ -9,15 +9,16 @@ public class CreateTransferDtoResponse {
 	private String recipientName;
 	
 	// Paynow
-	private String paynowPhoneNumber;
+	private String fromPaynowPhoneNumber;
+	private String toPaynowPhoneNumber;
 	
 	// Bank
 	private Instant transferDate;
-	private String bankName;
 	private String beneficiaryName;
 	
 	// Card and Bank
-	private String accountNumber;
+	private String fromAccountNumber;
+	private String toAccountNumber;
 	
 	// Generic
 	private Double transactionAmount;
@@ -25,101 +26,137 @@ public class CreateTransferDtoResponse {
 	private Long paymentMethodId;
 	private String remarks;
 	
-	public CreateTransferDtoResponse(Long transferId,
-			Double transactionAmount, String recipientName, Long paymentMethodId, 
-			Instant createdAt, Instant transferDate, String paynowPhoneNumber,
-			String accountNumber, String remarks, String bankName,
-			String beneficiaryName) {
+	public CreateTransferDtoResponse(Long transferId, String recipientName,
+			String fromPaynowPhoneNumber, String toPaynowPhoneNumber,
+			Instant transferDate, String beneficiaryName,
+			String fromAccountNumber, String toAccountNumber,
+			Double transactionAmount, Instant createdAt, Long paymentMethodId,
+			String remarks) {
 		super();
 		this.transferId = transferId;
-		this.transactionAmount = transactionAmount;
 		this.recipientName = recipientName;
-		this.createdAt = createdAt;
+		this.fromPaynowPhoneNumber = fromPaynowPhoneNumber;
+		this.toPaynowPhoneNumber = toPaynowPhoneNumber;
 		this.transferDate = transferDate;
-		this.paymentMethodId = paymentMethodId;
-		this.paynowPhoneNumber = paynowPhoneNumber;
-		this.accountNumber = accountNumber;
-		this.remarks = remarks;
-		this.bankName = bankName;
 		this.beneficiaryName = beneficiaryName;
+		this.fromAccountNumber = fromAccountNumber;
+		this.toAccountNumber = toAccountNumber;
+		this.transactionAmount = transactionAmount;
+		this.createdAt = createdAt;
+		this.paymentMethodId = paymentMethodId;
+		this.remarks = remarks;
 	}
+
 	public Long getTransferId() {
 		return transferId;
 	}
+
 	public void setTransferId(Long transferId) {
 		this.transferId = transferId;
 	}
-	public Double getTransactionAmount() {
-		return transactionAmount;
-	}
-	public void setTransactionAmount(Double transactionAmount) {
-		this.transactionAmount = transactionAmount;
-	}
+
 	public String getRecipientName() {
 		return recipientName;
 	}
+
 	public void setRecipientName(String recipientName) {
 		this.recipientName = recipientName;
 	}
-	public Instant getCreatedAt() {
-		return createdAt;
+
+	public String getFromPaynowPhoneNumber() {
+		return fromPaynowPhoneNumber;
 	}
-	public void setCreatedAt(Instant createdAt) {
-		this.createdAt = createdAt;
+
+	public void setFromPaynowPhoneNumber(String fromPaynowPhoneNumber) {
+		this.fromPaynowPhoneNumber = fromPaynowPhoneNumber;
 	}
-	public Long getPaymentMethodId() {
-		return paymentMethodId;
+
+	public String getToPaynowPhoneNumber() {
+		return toPaynowPhoneNumber;
 	}
-	public void setPaymentMethodId(Long paymentMethodId) {
-		this.paymentMethodId = paymentMethodId;
+
+	public void setToPaynowPhoneNumber(String toPaynowPhoneNumber) {
+		this.toPaynowPhoneNumber = toPaynowPhoneNumber;
 	}
-	
-	public String getPaynowPhoneNumber() {
-		return paynowPhoneNumber;
-	}
-	public void setPaynowPhoneNumber(String paynowPhoneNumber) {
-		this.paynowPhoneNumber = paynowPhoneNumber;
-	}
-	public String getAccountNumber() {
-		return accountNumber;
-	}
-	public void setAccountNumber(String accountNumber) {
-		this.accountNumber = accountNumber;
-	}
-	public String getRemarks() {
-		return remarks;
-	}
-	public void setRemarks(String remarks) {
-		this.remarks = remarks;
-	}
+
 	public Instant getTransferDate() {
 		return transferDate;
 	}
+
 	public void setTransferDate(Instant transferDate) {
 		this.transferDate = transferDate;
 	}
-	public String getBankName() {
-		return bankName;
-	}
-	public void setBankName(String bankName) {
-		this.bankName = bankName;
-	}
+
 	public String getBeneficiaryName() {
 		return beneficiaryName;
 	}
+
 	public void setBeneficiaryName(String beneficiaryName) {
 		this.beneficiaryName = beneficiaryName;
 	}
+
+	public String getFromAccountNumber() {
+		return fromAccountNumber;
+	}
+
+	public void setFromAccountNumber(String fromAccountNumber) {
+		this.fromAccountNumber = fromAccountNumber;
+	}
+
+	public String getToAccountNumber() {
+		return toAccountNumber;
+	}
+
+	public void setToAccountNumber(String toAccountNumber) {
+		this.toAccountNumber = toAccountNumber;
+	}
+
+	public Double getTransactionAmount() {
+		return transactionAmount;
+	}
+
+	public void setTransactionAmount(Double transactionAmount) {
+		this.transactionAmount = transactionAmount;
+	}
+
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Instant createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Long getPaymentMethodId() {
+		return paymentMethodId;
+	}
+
+	public void setPaymentMethodId(Long paymentMethodId) {
+		this.paymentMethodId = paymentMethodId;
+	}
+
+	public String getRemarks() {
+		return remarks;
+	}
+
+	public void setRemarks(String remarks) {
+		this.remarks = remarks;
+	}
+
 	@Override
 	public String toString() {
 		return "CreateTransferDtoResponse [transferId=" + transferId
-				+ ", recipientName=" + recipientName + ", paynowPhoneNumber="
-				+ paynowPhoneNumber + ", transferDate=" + transferDate
-				+ ", bankName=" + bankName + ", beneficiaryName="
-				+ beneficiaryName + ", accountNumber=" + accountNumber
+				+ ", recipientName=" + recipientName
+				+ ", fromPaynowPhoneNumber=" + fromPaynowPhoneNumber
+				+ ", toPaynowPhoneNumber=" + toPaynowPhoneNumber
+				+ ", transferDate=" + transferDate + ", beneficiaryName="
+				+ beneficiaryName + ", fromAccountNumber=" + fromAccountNumber
+				+ ", toAccountNumber=" + toAccountNumber
 				+ ", transactionAmount=" + transactionAmount + ", createdAt="
-				+ createdAt + ", paymentMethodId=" + paymentMethodId + ", remarks="
-				+ remarks + "]";
+				+ createdAt + ", paymentMethodId=" + paymentMethodId
+				+ ", remarks=" + remarks + "]";
 	}
+	
+	
 	
 }
